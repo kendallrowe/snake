84f1a8725949a14e891b994ff5b04fd90722b9f0
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-03 17:10:00 +0000

    Added name assignment upon connection for client

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function() {
     host: '172.46.0.110',
     port: 50541
   });
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: KEN");
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
